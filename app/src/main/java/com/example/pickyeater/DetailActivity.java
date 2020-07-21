d6d1f3d267e71450cadb04c9447744d27f3477e8
Ainsley O'Garro <aogarro1@fb.com>
2020-07-21 19:34:16 -0400

    Hours Added Hours Text to Detail Activity

--- app/src/main/java/com/example/pickyeater/DetailActivity.java
+++ app/src/main/java/com/example/pickyeater/DetailActivity.java
@@ -7,19 +7,128 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestHeaders;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
+
 public class DetailActivity extends AppCompatActivity {
+    private static final String REST_CONSUMER_SECRET = "SBsaWidRTfoXuVxS04YZE74ExQNX_cmIgNZTz2CH01W_BonLGCy1B0QTCTbkI_6aqvOWk4rG1hEpn2fbPWTHK3JrzleQAkQxcjni5HH48VaGAsga3LZuDkJ4rrMMX3Yx";
     private ImageView ivRestaurant;
     private TextView tvTitle;
     private TextView tvAddress;
     private RatingBar rbRestaurant;
+    private TextView tvHours;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        // Retrieve restaurant ID from click
-        getIntent().getStringExtra("id");
 
+       ivRestaurant = findViewById(R.id.ivRestaurantDetail);
+       tvTitle = findViewById(R.id.tvTitleDetail);
+       tvAddress = findViewById(R.id.tvAddressDetail);
+       rbRestaurant = findViewById(R.id.rbRestaurant);
+       tvHours = findViewById(R.id.tvHours);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        RequestHeaders authorization = new RequestHeaders();
+        authorization.put("Authorization", "Bearer " + REST_CONSUMER_SECRET);
+
+        String apiUrl = "https://api.yelp.com/v3/businesses/" +  getIntent().getStringExtra("id");;
+        RequestParams params = new RequestParams();
+
+        client.get(apiUrl, authorization, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONObject restaurant = json.jsonObject;
+                try {
+                    tvTitle.setText(restaurant.getString("name"));
+                    tvAddress.setText(restaurant.getJSONObject("location").getString("display_address"));
+                    rbRestaurant.setNumStars(((int) restaurant.getDouble("rating")));
+                    Glide.with(getApplicationContext()).load(restaurant.optString("image_url")).into(ivRestaurant);
+                    JSONArray hours = restaurant.getJSONArray("hours").getJSONObject(0).getJSONArray("open");
+                    for (int i = 0; i < hours.length() ; i++) {
+                        JSONObject day = hours.getJSONObject(i);
+                        tvHours.setText(tvHours.getText() + HoursConverter(day.getString("start"), day.getString("end"), day.getInt("day")) + "\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+    }
+
+    public String HoursConverter(String start, String end, int day){
+        String open = MilitarytoNormal(start);
+        String close = MilitarytoNormal(end);
+        if (day == 0){
+            return "Mon " + open + "-" + close;
+        }
+
+        else if (day == 1){
+            return "Tues" + open + "-" + close;
+        }
+
+        else if (day == 2){
+            return "Wed " + open + "-" + close;
+        }
+        else if (day == 3){
+            return "Thurs" + open + "-" + close;
+        }
+
+        else if (day == 4){
+            return "Tues" + open + "-" + close;
+        }
+
+        else if (day == 5){
+            return "Fri " + open + "-" + close;
+        }
+        else {
+            return "Sat" + open + "-" + close;
+        }
+
+
+    }
+
+    public String MilitarytoNormal(String time){
+        String newtime = "";
+        String first2digits = time.substring(0,2);
+        String last2digits = time.substring(2,4);
+        if (Integer.valueOf(first2digits) < 12){
+            if (first2digits.equals("00")){
+                newtime = "12" + ":" + last2digits + "AM";
+            }
+            else if (Integer.valueOf(first2digits) <  10){
+                newtime = first2digits.substring(1) + ":" + last2digits + "AM";
+            }
+            else{
+                newtime = first2digits + ":" + last2digits + "AM";
+            }
+        }
+        else{
+            if (Integer.valueOf(first2digits) == 12){
+                newtime = first2digits + ":" + last2digits + "PM";
+            }
+
+            else{
+                newtime = (Integer.valueOf(first2digits) - 12) + ":" + last2digits + "PM";
+            }
+        }
+        return newtime;
     }
 }
